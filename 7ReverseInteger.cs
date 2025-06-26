172ca9de6db349b2382f2bf4f234ddc2f6e159ae
dylanturnerbrinson <89657426+dylanturnerbrinson@users.noreply.github.com>
2025-06-25 23:05:24 -0500

    Add files via upload

---
+++ 7ReverseInteger.cs
@@ -0,0 +1,35 @@
+﻿//Reverse integer
+
+public class Solution
+{
+    public int Reverse(int x)
+    {
+        bool neg = x < 0;
+
+        if (neg)
+        {
+            x = x * -1;
+        }
+
+        string strX = x.ToString();
+
+        if (neg)
+        {
+            strX += "-";
+        }
+
+        char[] charX = strX.ToArray();
+
+        Array.Reverse(charX);
+
+        strX = string.Join("", charX);
+
+        int ans = 0;
+
+        //Console.WriteLine($"reversed int : {strX}");
+
+        Int32.TryParse(strX, out ans);
+
+        return ans;
+    }
+}
