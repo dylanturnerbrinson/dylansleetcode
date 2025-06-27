39ef57ada1f8e0aefe1fa4edcbef917db2450b70
dylanturnerbrinson <89657426+dylanturnerbrinson@users.noreply.github.com>
2025-06-27 17:50:37 -0500

    Add files via upload

---
+++ 26RemoveDuplicatesfromSortedArray.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] == nums[i + 1] && nums[i] != 101)
+            {
+                for (int x = i; x < nums.Length - 1; x++)
+                {
+                    nums[x] = nums[x + 1];
+                    nums[x + 1] = 101;
+                }
+                i--;
+            }
+            else if (nums[i] == 101)
+            {
+                break;
+            }
+        }
+
+        nums = nums.Where(x => x != 101).ToArray();
+        int ans = Array.IndexOf(nums, 101);
+
+        return nums.Count();
+    }
+}
